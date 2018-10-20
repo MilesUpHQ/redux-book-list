5f14b7c36ea9bc60639a32a91f9c59ba19e07ae0
Magesh <magesh@hash14.com>
2018-10-20 13:57:59 +0530

    Passing state as props

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -4,8 +4,9 @@
 //Container is just a component that has direct access to the state that's produced by redux
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component{
+class BookList extends Component{
   renderList(){
     return this.props.books.map((book) => {
       return(
@@ -14,10 +15,19 @@ export default class BookList extends Component{
     });
   }
   render(){
+    console.log(this.props.asdf); // -> '123'
     return(
       <ul className="list-group col-sm-4">
         {this.renderList()}
       </ul>
     )
   }
+}
+
+function mapStateToProps(state){
+  // Takes the state of the application as argument
+  // whatever is returned will show up as props inside of BookList
+  return {
+    asdf: '123'
+  };
 }
