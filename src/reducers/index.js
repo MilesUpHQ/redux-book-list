3fe0500e8877150b467cc3a2e7883f43d6ad9485
Magesh <magesh@hash14.com>
2018-10-20 13:17:48 +0530

    created a books reducer and wired it with combileReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
+// Reducer is a function that returns a piece of the application state
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
