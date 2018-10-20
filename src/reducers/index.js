4a170666df5953a49d8865a9c8f5efefddf048a6
Magesh <magesh@hash14.com>
2018-10-20 23:54:59 +0530

    created active book reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 // Reducer is a function that returns a piece of the application state
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book'
 
 const rootReducer = combineReducers({
   //state: (state = {}) => state
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
